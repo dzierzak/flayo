260237f74acbc7398c0d2e765c73490190e56d74
Dzierzak <mateusz@dzierzecki.com>
2018-03-31 23:56:21 +0200

    code cleaned

--- joboffers/models.py
+++ joboffers/models.py
@@ -95,17 +95,3 @@ class Offer(models.Model):
     def __str__(self):
         return self.position
 
-#
-# class Category(models.Model):
-#     name = models.CharField(max_length=64, blank=False)
-#
-#     def __str__(self):
-#         return self.name
-#
-#
-# class Subcategory(models.Model):
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-#     name = models.CharField(max_length=64, blank=False)
-#
-#     def __str__(self):
-#         return self.name
